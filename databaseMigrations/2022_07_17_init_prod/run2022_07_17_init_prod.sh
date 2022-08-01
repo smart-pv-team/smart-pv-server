8e6b6fbd21ff5930417186bc64d24a2612716c8a
JanickiJ <kubajanicki244@gmail.com>
2022-08-02 00:50:09 +0200

    changed db url

--- databaseMigrations/2022_07_17_init_prod/run2022_07_17_init_prod.sh
+++ databaseMigrations/2022_07_17_init_prod/run2022_07_17_init_prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
 #TODO hide db conn credentials
 
-mongosh "mongodb+srv://cluster0.qd1lc.mongodb.net/myFirstDatabase" --username kuba 2022_07_17_init_prod.js
+mongosh "mongodb+srv://smartpv.4jizccr.mongodb.net/myFirstDatabase" --apiVersion 1 --username janicki --password janicki 2022_07_17_init_prod.js
