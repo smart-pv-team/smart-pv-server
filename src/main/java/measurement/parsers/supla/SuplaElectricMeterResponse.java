745af2ce9adbfd6bf6df784214e9efc3898e28b2
JanickiJ <kubajanicki244@gmail.com>
2022-08-15 19:30:02 +0200

    Checkstyle error

--- src/main/java/measurement/parsers/supla/SuplaElectricMeterResponse.java
+++ src/main/java/measurement/parsers/supla/SuplaElectricMeterResponse.java
@@ -3,8 +3,8 @@ package measurement.parsers.supla;
 import java.util.Currency;
 import java.util.Date;
 import java.util.List;
-import measurement.persistence.MeasurementEntity;
 import measurement.parsers.Response;
+import measurement.persistence.MeasurementEntity;
 
 public record SuplaElectricMeterResponse(boolean connected, Float support, Currency currency,
                                          Float pricePerUnit, Float totalCost,
@@ -13,7 +13,7 @@ public record SuplaElectricMeterResponse(boolean connected, Float support, Curre
   @Override
   public MeasurementEntity toMeasurementEntity(String id) {
     float measurement = (float) phases.stream().mapToDouble(PhaseResponse::powerActive).sum();
-    float reversedMeasurement = - measurement;
+    float reversedMeasurement = -measurement;
     return new MeasurementEntity(id, reversedMeasurement, new Date());
   }
 }
