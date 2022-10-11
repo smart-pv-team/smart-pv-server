585a522ff50135e8425f9273918f9f6fa3e6142d
JanickiJ <kubajanicki244@gmail.com>
2022-10-11 16:08:07 +0200

    added last status change

--- src/main/java/smartpv/consumption/ControlParameters.java
+++ src/main/java/smartpv/consumption/ControlParameters.java
@@ -4,11 +4,15 @@ import java.util.Date;
 import smartpv.server.utils.DateTimeUtils;
 
 public record ControlParameters(Integer priority, Float powerConsumption, Integer minHysteresis,
-                                Integer maxHysteresis, Lock lock) {
+                                Integer maxHysteresis, Lock lock, Date lastStatusChange) {
 
   public ControlParameters withLock(Boolean status) {
     return new ControlParameters(priority, powerConsumption, minHysteresis, maxHysteresis,
-        new Lock(status, DateTimeUtils.endOfDay()));
+        new Lock(status, DateTimeUtils.endOfDay()), lastStatusChange);
+  }
+
+  public ControlParameters withLastStatusChange(Date lastStatusChange) {
+    return new ControlParameters(priority, powerConsumption, minHysteresis, maxHysteresis, lock, lastStatusChange);
   }
 
   public record Lock(Boolean isLocked, Date date) {
