4c91a6245482003801df03c8faf992fb0718ad83
JanickiJ <kubajanicki244@gmail.com>
2022-11-11 23:14:36 +0100

    changed ci

--- Adapters/src/main/java/com/adapters/outbound/http/devices/ConsumptionOnOff.java
+++ Adapters/src/main/java/com/adapters/outbound/http/devices/ConsumptionOnOff.java
@@ -3,7 +3,7 @@ package com.adapters.outbound.http.devices;
 import com.domain.model.actions.ConsumptionOnOffAction;
 import com.domain.model.consumption.ConsumptionDevice;
 
-public interface ConsumptionOnOff {
+public interface ConsumptionOnOff extends Response {
 
   ConsumptionOnOffAction toDomain(ConsumptionDevice consumptionDevice);
 }
--- Adapters/src/main/java/com/adapters/outbound/http/devices/MeasurementRead.java
+++ Adapters/src/main/java/com/adapters/outbound/http/devices/MeasurementRead.java
@@ -2,9 +2,8 @@ package com.adapters.outbound.http.devices;
 
 import com.domain.model.actions.MeasurementReadAction;
 import com.domain.model.measurement.MeasurementDevice;
-import java.io.Serializable;
 
-public interface MeasurementRead extends Serializable {
+public interface MeasurementRead extends Response {
 
   MeasurementReadAction toDomain(MeasurementDevice measurementDevice);
 }
